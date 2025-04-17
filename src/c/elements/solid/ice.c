69152a5f93973231c621ecb3a79b8c9fc56c0691
warrengalyen <wgalyen@hotmail.com>
2025-04-17 10:07:32 -0700

    add solid elements

---
+++ src/c/elements/solid/ice.c
@@ -0,0 +1,32 @@
+#include "../../color.h"
+#include "../../fluidsim.h"
+#include "../../main.h"
+#include "../../random.h"
+#include "../elements.h"
+
+#define ELEMENT_NAME ice
+
+_Bool VARPREF(attempt)(Element *el, Cell *cell, Cell *target) {
+    return default_attempt(el, cell, target);
+}
+
+void VARPREF(update)(Element *el, Cell *cell, U16 x, U16 y) {
+    APPROACHIFMORE(fluid->density[cell->fluidInd], -5.1f, 0.98);
+    if (cell->temperature >= 0.6f) {
+        freeCell(cell);
+        spawnElement(cell, WATER);
+        return;
+    }
+}
+
+ElementInfo VARPOST(info) = {
+                             .handler = VARPREF(update),
+                             .attempt = VARPREF(attempt),
+
+                             .state = s_SOLID,
+                             .weight = 4,
+                             .isStatic = 1,
+                             .explosionResistance = 30,
+                             .baseColor = TOHSL(206, 40, 63)};
+
+#undef ELEMENT_NAME
