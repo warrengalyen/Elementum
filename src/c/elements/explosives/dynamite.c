b207e1e79c6035563cef4cf499e7abc7733a8274
warrengalyen <wgalyen@hotmail.com>
2025-04-19 11:58:36 -0700

    add explosive elements

---
+++ src/c/elements/explosives/dynamite.c
@@ -0,0 +1,28 @@
+#include "../../color.h"
+#include "../../fluidsim.h"
+#include "../../main.h"
+#include "../../random.h"
+#include "../elements.h"
+
+
+#define ELEMENT_NAME dynamite
+
+void VARPREF(update)(Element *el, Cell *cell, U16 x, U16 y) {
+    if (el->r0 || el->electricityState) {
+        freeCell(cell);
+        explode(x, y, el->rv % 30);
+    }
+}
+
+ElementInfo VARPOST(info) = {
+                             .handler = VARPREF(update),
+
+                             .temp = (TemperatureInfo){.insolation = 1.0f, .flammable = 1, .burnTime = 30},
+
+                             .state = s_SOLID,
+                             .weight = 30,
+                             .isStatic = 1,
+                             .explosionResistance = 8,
+                             .baseColor = TOHSL(0, 73, 63)};
+
+#undef ELEMENT_NAME
